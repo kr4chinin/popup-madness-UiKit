62ac3160188d93b1a951f7148c60975ae48b55bc
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-19 16:27:37 +0300

    style: applied global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<App />
