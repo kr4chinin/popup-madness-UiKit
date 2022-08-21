13ddd5c8688ca1cccca5d1cbd452a4e53c61f53d
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-21 14:33:06 +0300

    feat: information pop-ups after status editing

---
+++ src/components/StatusSuccessPopup/StatusSuccessPopup.tsx
@@ -0,0 +1,27 @@
+import { FC, useState } from 'react'
+import Dialog from '../UiKit/Dialog/Dialog'
+import styles from './StatusSuccessPopup.module.scss'
+
+interface StatusSuccessPopupProps {
+	isOpened: boolean
+	onClose: () => void
+}
+
+const StatusSuccessPopup: FC<StatusSuccessPopupProps> = ({
+	isOpened,
+	onClose
+}) => {
+	return (
+		<Dialog
+			id="3"
+			isOpened={isOpened}
+			onClose={onClose}
+			primaryButtonText="Continue"
+			primaryButtonOnClick={onClose}
+		>
+			<p className={styles.message}>✅ New status applied successfully!</p>
+		</Dialog>
+	)
+}
+
+export default StatusSuccessPopup
