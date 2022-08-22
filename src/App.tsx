252a5d1d6a95e707d525ce9bb4e3018315f86a6a
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-22 13:28:16 +0300

    fix: changed user ids

--- src/App.tsx
+++ src/App.tsx
@@ -14,30 +14,30 @@ const App = () => {
 	}
 
 	const [user, setUser] = useState<User>({
-		id: '1',
+		id: 'user-1',
 		avatar: '👤',
 		nickname: 'Anonimous2002',
 		status: Status.DO_NOT_DISTURB,
 		bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis sequi non, id magnam dolore inventore consequatur! A quidem ab assumenda id dolor soluta reiciendis neque, vero blanditiis consectetur sit rerum sapiente voluptas! Eum beatae pariatur sapiente est quae. Sunt, ad fugiat optio similique neque ipsa sit dolorum illo iure, repudiandae eaque doloremque deleniti enim autem, omnis at eligendi qui nihil? Exercitationem, quos tenetur mollitia corporis incidunt ratione tempora, perferendis reiciendis aliquid eaque, similique minus? Perferendis quo aliquam maxime ratione facilis veniam reprehenderit dicta labore repudiandae necessitatibus! Impedit quas magni, id beatae exercitationem quisquam sapiente officiis placeat error! Quisquam unde aspernatur odio doloremque iste, nostrum deserunt magnam quidem aliquid soluta vero perspiciatis ea corrupti sunt praesentium iure. Alias, ipsum omnis. Ipsum laborum excepturi repudiandae eligendi amet aliquam at hic fuga fugit? Excepturi et perferendis voluptatibus sapiente rem assumenda totam culpa mollitia voluptatum eveniet dolores similique fugiat ducimus distinctio, dolorem reprehenderit quo.',
 		friends: [
 			{
-				id: '2',
+				id: 'user-2',
 				avatar: '👨🏻‍🦱',
-				nickname: 'MaximusRobot',
+				nickname: 'Maximus_Robot',
 				status: Status.ONLINE,
 				bio: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab velit aliquam obcaecati in sequi. Cum, molestiae. Harum repudiandae ipsam id optio debitis iusto iure fugit, et similique possimus, sit veritatis vitae eligendi delectus facere minus facilis voluptas officia. Quos.',
 				friends: []
 			},
 			{
-				id: '3',
-				avatar: '🧓🏼',
-				nickname: '180Killer',
+				id: 'user-3',
+				avatar: '🧑🏻‍💻',
+				nickname: '180-Killer',
 				status: Status.OFFLINE,
-				bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum aliquam suscipit excepturi nobis impedit nulla perspiciatis fuga deserunt molestias sed consequatur a maiores esse, voluptatem facere eius dicta explicabo qui iure tempora quo. Obcaecati ad totam, optio pariatur non beatae? Iure architecto ipsam quisquam reprehenderit. Impedit nihil inventore provident!',
+				bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum aliquam suscipit excepturi nobis impedit nulla.',
 				friends: []
 			},
 			{
-				id: '4',
+				id: 'user-4',
 				avatar: '🕵🏼',
 				nickname: 'AndrewCoolX',
 				status: Status.PLAYING,
@@ -45,7 +45,7 @@ const App = () => {
 				friends: []
 			},
 			{
-				id: '5',
+				id: 'user-5',
 				avatar: '👨🏻‍🍳',
 				nickname: 'XxJohnxX',
 				status: Status.WORKING,
@@ -53,8 +53,8 @@ const App = () => {
 				friends: []
 			},
 			{
-				id: '6',
-				avatar: '🧑🏻‍💻',
+				id: 'user-6',
+				avatar: '🧓🏼',
 				nickname: 'Queen2003',
 				status: Status.DO_NOT_DISTURB,
 				bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga suscipit distinctio sequi voluptates nostrum, eum, itaque fugiat quae facere earum consectetur nisi modi quo eius qui aliquam iure, sit explicabo pariatur deleniti ut architecto? Doloremque, rem nihil. Quam tempore, sit recusandae laboriosam, non accusamus eligendi mollitia incidunt voluptatibus minima minus. Debitis excepturi vero atque velit corrupti ipsum voluptate perspiciatis expedita sed impedit cum quisquam, alias error harum sapiente provident at inventore. Quibusdam veniam in esse quae maiores rerum error obcaecati cupiditate laudantium totam vitae excepturi enim nostrum officia fuga laborum deleniti eos ipsum officiis, impedit suscipit! Voluptates distinctio porro minus!',
