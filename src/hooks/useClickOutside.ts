57f7c5d2560d07226025d93cc36d2e41e94919ef
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-25 15:32:53 +0300

    feat: prevent fire func on elementRef's children

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,19 +1,29 @@
 import { useEffect } from 'react'
 
 export function useClickOutside(
-	popperRef: HTMLElement,
-	elementRef: HTMLElement,
+	popperRef: HTMLElement | null,
+	elementRef: HTMLElement | null,
 	func: () => void
 ) {
 	useEffect(() => {
-		function handleClickOutside(e: any) {
+		function handleClickOutside(e: MouseEvent) {
 			if (e.target === elementRef) {
 				return
 			}
-			if (popperRef && !popperRef.contains(e.target)) {
+
+            if (elementRef && elementRef.children) {
+                for (let i = 0; i < elementRef.children.length; i++) {
+                    if (e.target === elementRef.children[i]) {
+                        return
+                    }
+                }
+            }
+
+			if (popperRef && !popperRef.contains(e.target as Node)) {
 				func()
 			}
 		}
+        
 		document.addEventListener('mousedown', handleClickOutside)
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside)
